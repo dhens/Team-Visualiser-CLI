5416bdfa464b07c3f2f06285c1e6c457d23d1364
dhens <dawsonhensel@gmail.com>
2020-02-21 16:36:51 -0800

    added inquirer that asks all required questions, including question branching depending on user-entered role type. once all questions are answered, a new Manager, Engineer, or Intern with data supplied by user

--- app.js
+++ app.js
@@ -4,32 +4,73 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const questions = 
+    [
+        {
+            type: 'input',
+            message: 'Enter your name:',
+            name: 'Name'
+        },
+        {
+            type: 'list',
+            message: 'Enter your role:',
+            choices: ['Manager', 'Engineer', 'Intern'],
+            name: 'Role'
+        },
+        {
+            type: 'input',
+            message: 'Enter your email:',
+            name: 'Email'
+        },
+        {
+            type: 'input',
+            message: 'Enter your ID number:',
+            name: 'Id'
+        }
+    ];
+
+
+inquirer.prompt(questions)
+    .then(function(reqQuestions) {
+        switch(reqQuestions.Role) {
+            case 'Manager':
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'Enter your office number',
+                        name: 'officeNum'
+                    }])
+                    .then(function(res) {
+                        console.log(`User entered: ${res.officeNum}`);
+                        new Manager(reqQuestions.name, reqQuestions.Id, reqQuestions.Email, res.officeNum);
+                    })
+                break;
+            case 'Engineer':
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'Enter your GitHub username',
+                        name: 'github'
+                    }]).then(function(res) {
+                        console.log(res, reqQuestions);
+                        console.log(`User entered: ${res.github}`);
+                        new Engineer(reqQuestions.name, reqQuestions.Id, reqQuestions.Email, res.github);
+                    })
+                break;
+            case 'Intern':
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'Enter your school',
+                        name: 'school'
+                    }])
+                    .then(function(res) {
+                        console.log(`User entered: ${res.school}`);
+                        new Intern(reqQuestions.name, reqQuestions.Id, reqQuestions.Email, res.school);
+                    })
+                break;        
+            }
+});
