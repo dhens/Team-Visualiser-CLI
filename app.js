504798ffdfbc89d012f4ce280b48db1d18fd6aaf
Dawson Hensel <dawsonhensel@gmail.com>
2020-02-24 14:50:26 -0800

    added logic to ask for as many team members as the user wants, and keeps asking until they say they are done. stores all created Eployees into an array for later usage

--- app.js
+++ app.js
@@ -32,45 +32,90 @@ const questions =
         }
     ];
 
+let teamArr = [];
 
-inquirer.prompt(questions)
-    .then(function(reqQuestions) {
-        switch(reqQuestions.Role) {
-            case 'Manager':
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        message: 'Enter your office number',
-                        name: 'officeNum'
-                    }])
-                    .then(function(res) {
-                        console.log(`User entered: ${res.officeNum}`);
-                        new Manager(reqQuestions.name, reqQuestions.Id, reqQuestions.Email, res.officeNum);
-                    })
-                break;
-            case 'Engineer':
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        message: 'Enter your GitHub username',
-                        name: 'github'
-                    }]).then(function(res) {
-                        console.log(res, reqQuestions);
-                        console.log(`User entered: ${res.github}`);
-                        new Engineer(reqQuestions.name, reqQuestions.Id, reqQuestions.Email, res.github);
-                    })
-                break;
-            case 'Intern':
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        message: 'Enter your school',
-                        name: 'school'
-                    }])
-                    .then(function(res) {
-                        console.log(`User entered: ${res.school}`);
-                        new Intern(reqQuestions.name, reqQuestions.Id, reqQuestions.Email, res.school);
-                    })
-                break;        
+function addUser() {
+    inquirer.prompt(questions)
+        .then(function(reqQuestions) {
+            switch(reqQuestions.Role) {
+                case 'Manager':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter your office number',
+                            name: 'officeNum'
+                        }])
+                        .then(function(res) {
+                            teamArr.push(new Manager(
+                                reqQuestions.Name, 
+                                reqQuestions.Id, 
+                                reqQuestions.Email, 
+                                res.officeNum));
+                                shouldYouAddAnotherUser();
+                        }); 
+                    break;
+                case 'Engineer':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter your GitHub username',
+                            name: 'github'
+                        }]).then(function(res) {
+                            console.log(`User entered: ${res.github}`);
+                            teamArr.push(new Engineer(
+                                reqQuestions.Name, 
+                                reqQuestions.Id, 
+                                reqQuestions.Email, 
+                                res.github));
+                                shouldYouAddAnotherUser();
+                        })
+                    break;
+                case 'Intern':
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter your school',
+                            name: 'school'
+                        }])
+                        .then(function(res) {
+                            console.log(`User entered: ${res.school}`);
+                            teamArr.push(new Intern(
+                                reqQuestions.Name, 
+                                reqQuestions.Id, 
+                                reqQuestions.Email, 
+                                res.school));
+                                shouldYouAddAnotherUser();
+                        });
+                    break;        
             }
-});
+    });
+}
+function shouldYouAddAnotherUser() {
+    inquirer.prompt(
+        [
+            {
+                type: "confirm",
+                message: "Add another user?",
+                name: 'addUsrQuestion'
+            }
+        ]
+    ).then(function(res) {
+        console.log(res.addUsrQuestion);
+        if(res.addUsrQuestion) {
+            addUser();
+        } else {
+            console.log(`Your team array consists of: ${JSON.stringify(teamArr)}`)
+            console.log("Generating HTML! Exiting...")
+            // genHTML()
+        }
+    })
+    // ask questiion do you want to add additonal users
+    // if answer === yes, re-run creaatesuer function
+    // if no, genearte html page and exit process
+}
+
+function init() {
+    addUser();
+}
+
+init();
